5898bc8e75eef7cf8622f0e1883bcf228be437eb
ethan-luxton <luxtonethan@hotmail.com>
2022-07-17 22:06:19 -0700

    Added congrats message for getting all questions right!

--- js/app.js
+++ js/app.js
@@ -7,50 +7,46 @@ let q4 = prompt('Does Ethan work in finance? (Yes or No)(y/n)');
 let q5 = prompt('Does Ethan take portrait photos? (Yes or No)(y/n)');
 let q6 = prompt('Go cougs? (Yes or No)(y/n)');
 
-function questionnaire1() {
-    let y = 'yes' && 'y';
-    let n = 'no' && 'n';
-    const answArray = [];
-    const answ = answArray.push(q2, q3, q4, q5, q6);
-    if (answArray[0, 2, 4] == y && answArray[1, 3] == n) {
-        alert('That\'s correct!');
+
+let y = 'yes' && 'y';
+let n = 'no' && 'n';
+const answArray = [];
+const answ = answArray.push(q2, q3, q4, q5, q6);
+if (answArray[0, 2, 4] == y && answArray[1, 3] == n) {
+    alert('That\'s correct!');
+} else {
+    alert('That\'s not correct :( read my about me page!');
+}
+let i = 5;
+let correctNum = 8;
+const warmCold = ['Colder', 'Warmer'];
+let correctShow = 'stranger things';
+while (i > 0) {
+    let q7 = prompt('Guess a number between 1-25.');
+    i--;
+    if (q7 != correctNum) {
+        if (q7 < 6 || q7 > 10) {
+            alert('That\'s not correct! Try again! ' + i + ' tries left! You are: ' + warmCold[0]);
+        } else if (q7 == 6 || q7 == 7 || q7 == 8 || q7 == 9) {
+            alert('That\'s not correct! Try again! ' + i + ' tries left! You are: ' + warmCold[1]); 
+        }
     } else {
-        alert('That\'s not correct :( read my about me page!');
+        alert('That\'s correct!')
+    }
+    let q8 = prompt('What do you think my favorite show is?');
+    i--;
+    if (q8 != correctShow) {
+        alert('That\'s not correct! Try again! ' + i + ' tries left!'); 
+    } else if (q8 == correctShow) {
+        alert('That\'s correct!');
+        if (answArray[0, 2, 4] == y && answArray[1, 3] == n && q7 == correctNum && q8 == correctShow) {
+            alert("Congrats! You got all the questions right!")
+        }
+        break;
     }
 }
-questionnaire1();
-function questionnaire2() {
-    let i = 5;
-    let correctNum = 8;
-    const warmCold = ['Colder', 'Warmer']
-    let correctShow = 'stranger things';
-    while (i > 0) {
-        let q7 = prompt('Guess a number between 1-25.');
-        i--;
-        if (q7 != correctNum) {
-            if (q7 < 6 || q7 > 10) {
-                alert('That\'s not correct! Try again! ' + i + ' tries left! You are: ' + warmCold[0]);
-            } else if (q7 == 6 || q7 == 7 || q7 == 8 || q7 == 9) {
-                alert('That\'s not correct! Try again! ' + i + ' tries left! You are: ' + warmCold[1]); 
-            }
-        } else {
-            alert('That\'s correct!')
-            break;
-        }
-    }
-    while (i > 0) {
-        let q8 = prompt('What do you think my favorite show is?');
-        i--;
-        if (q8 != correctShow) {
-            alert('That\'s not correct! Try again! ' + i + ' tries left!'); 
-        } else if (q8 == correctShow) {
-            alert('That\'s correct!');
-            break;
-        }
-    }
-}
-    
-questionnaire2();
+
+
 
 // function displayUserName() {
 //     document.write('Hello ' + userName + '!'); will use this function at a later date
